cdaad98592704e78149194dcf21ac514f216b37a
iamsiddharath <onlinesid91096@gmail.com>
2020-08-21 08:45:01 +0530

    initial-commit

---
+++ com.github.iamsiddharath.capture-me/src/com/github/iamsiddharath/captureme/handlers/CaptureMeHandler.java
@@ -0,0 +1,21 @@
+package com.github.iamsiddharath.captureme.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class CaptureMeHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Capture-me",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
