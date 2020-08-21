2d87319f15d95c609e70e704e97b530d8c3d90b8
iamsiddharath <onlinesid91096@gmail.com>
2020-08-21 10:24:39 +0530

    screen capture feature added

--- com.github.iamsiddharath.capture-me/src/com/github/iamsiddharath/captureme/handlers/CaptureMeHandler.java
+++ com.github.iamsiddharath.capture-me/src/com/github/iamsiddharath/captureme/handlers/CaptureMeHandler.java
@@ -1,5 +1,15 @@
 package com.github.iamsiddharath.captureme.handlers;
 
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -11,11 +21,36 @@ public class CaptureMeHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Capture-me", 
-				"Hello, Eclipse world");
+		try {
+			IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+			boolean flag = MessageDialog.openConfirm(window.getShell(), "Capture Me",
+					"Do you want to capture the screen ? ");
+
+			if (flag) {
+				Thread.sleep(1000);
+				Robot r = new Robot(); 
+				String filePath = System.getProperty("user.home")+"/Desktop/Screen-capture/";
+	            SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_sss");
+				
+				File folder = new File(filePath);
+				
+				if(!folder.exists()) {
+					folder.mkdir();
+				}
+				
+				
+	            String path = filePath+"Shot_"+sdf.format(new Date())+".jpg"; 
+	  
+	            Rectangle capture =  
+	            new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); 
+	            BufferedImage Image = r.createScreenCapture(capture); 
+	            ImageIO.write(Image, "jpg", new File(path));
+	            MessageDialog.openInformation(window.getShell(), "Capture Me",
+						"Success! Screenshot is captured \n\n Location of Screen shot : "+path);
+			}
+		} catch (Exception e) {
+		}
+
 		return null;
 	}
 }
